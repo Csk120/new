ca55b141de37a0a3d8eff1f9cf9e9c9e2c611c3f
Gaurav0844 <ranganekarg@gmail.com>
2022-04-07 19:05:44 +0530

    datatype

---
+++ datatype/src/module-info.java
@@ -0,0 +1,16 @@
+module datatype {
+}
+class apple
+{
+ int d=29; //instance variable
+ static double h =189.58; //static variable
+ public static void main(String[] args)
+ {
+     boolean c=true;
+     apple a=new apple();
+     System.out.println(a.d+" "+h+" "+c);
+     
+     
+     
+ }
+}
